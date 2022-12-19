1db0867f38c143a37673ec8a2326350383738d03
cityhighrider <60808209+city-high-rider@users.noreply.github.com>
2022-12-20 10:52:01 +1300

    write route parser and urltoroute function

--- src/Route.elm
+++ src/Route.elm
@@ -1,9 +1,40 @@
 -- the idea in this module is to convert our url routes from strings into
--- custom types, so that we are less likely to make a mistake while doing 
+-- custom types, so that we are less likely to make a mistake while doing
 -- routing
+
+
 module Route exposing (..)
 
+import Url exposing (Url)
+import Url.Parser exposing (Parser, map, s, oneOf, parse, top)
+
+
+
 -- This contains the pages that we can route to
-type Route 
+
+
+type Route
     = NotFoundPage
     | HomePage
+    | LevelViewPage
+
+
+-- route parser
+
+
+routeParser : Parser ( Route -> a ) a
+routeParser =
+    oneOf
+        [ map HomePage top
+        , map LevelViewPage (s "levels")
+        ]
+
+
+
+-- Url to route
+
+
+urlToRoute : Url -> Route
+urlToRoute url =
+    Maybe.withDefault NotFoundPage <|
+        parse routeParser url
