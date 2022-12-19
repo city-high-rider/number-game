52b34f406d19a9685fa58a6d97616f5d3acbabc8
cityhighrider <60808209+city-high-rider@users.noreply.github.com>
2022-12-20 11:24:00 +1300

    modify home page and link it to levels list page

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -1,10 +1,55 @@
-module Pages.Home exposing (..)
-
-import Html exposing (Html, div, h3, p, text)
+-- this is a home page with some info about the site
 
 
-main : Html msg
-main =
+module Pages.Home exposing (Model, Msg, init, update, view)
+
+import Browser.Navigation as Nav exposing (Key)
+import Html exposing (Html, button, div, h2, h3, p, text)
+import Html.Events exposing (onClick)
+import Url exposing (Url)
+import Route exposing (Route(..), pushUrl)
+
+
+
+-- model and init
+
+
+type alias Model =
+    { navKey : Nav.Key
+    }
+
+
+init : Nav.Key -> ( Model, Cmd Msg )
+init navkey =
+    ( Model navkey, Cmd.none )
+
+
+
+-- view function
+
+
+view : Model -> Html Msg
+view _ =
     div []
-        [ h3 [] [ text "Home page" ]
+        [ h2 [] [ text "Number game" ]
+        , h3 [] [ text "How to play : " ]
+
+        -- elm formmater turns this into a massive one line string
+        , p [] [ text "You are given a starting number and an ending number, as well as \n     buttons that perform arithmetic on your number. the goal is to get to the \n     ending number in the least amount of moves. There is a minimum amount of moves\n     required to pass the level." ]
+        , button [ onClick GotoLevelsPage ] [ text "View all levels" ]
         ]
+
+
+
+-- update and msg
+
+
+type Msg
+    = GotoLevelsPage
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotoLevelsPage ->
+            (model, Route.pushUrl LevelViewRoute model.navKey )
