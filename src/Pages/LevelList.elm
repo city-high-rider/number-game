ed6327e004569b671ee4af891e2b70226ac20fb4
cityhighrider <60808209+city-high-rider@users.noreply.github.com>
2022-12-21 11:13:37 +1300

    create level list page

--- src/Pages/LevelList.elm
+++ src/Pages/LevelList.elm
@@ -1,7 +1,127 @@
-module Pages.LevelList exposing (..)
+module Pages.LevelList exposing (Model, Msg, init, update, view)
 
-import Html exposing (text, Html)
+import Browser.Navigation exposing (Key)
+import Html exposing (Html, a, button, div, h3, p, table, td, text, th, tr)
+import Html.Attributes exposing (href)
+import Html.Events exposing (onClick)
+import Http
+import Level exposing (Level, levelIdToString, levelsDecoder)
+import RemoteData exposing (RemoteData(..), WebData)
 
-main : Html msg
-main =
-    text "placeholder"
+
+
+-- model and init
+
+
+type alias Model =
+    { levels : WebData (List Level)
+    , navKey : Key
+    }
+
+
+init : Key -> ( Model, Cmd Msg )
+init key =
+    let
+        initialModel =
+            Model Loading key
+    in
+    ( initialModel, fetchLevels )
+
+
+
+-- view function
+
+
+view : Model -> Html Msg
+view model =
+    case model.levels of
+        NotAsked ->
+            div []
+                [ h3 [] [ text "Nobody's asked for the levels to be loaded yet!" ]
+                , button [ onClick ManualLevelLoad ] [ text "Load levels" ]
+                ]
+
+        Loading ->
+            h3 [] [ text "Loading levels, please wait..." ]
+
+        Success levels ->
+            viewLevels levels
+
+        Failure error ->
+            viewError error
+
+
+viewLevels : List Level -> Html Msg
+viewLevels levels =
+    let
+        tableHeader =
+            tr []
+                [ th [] [ text "Level ID" ]
+                , th [] [ text "Difficulty" ]
+                ]
+    in
+    table []
+        (tableHeader :: List.map levelToTableRow levels)
+
+
+levelToTableRow : Level -> Html Msg
+levelToTableRow level =
+    tr []
+        [ td [] [ text (levelIdToString level.id) ]
+        , td [] [ text level.difficulty ]
+        , a [ href ("/levels/" ++ levelIdToString level.id) ] [ text "Play" ]
+        ]
+
+
+viewError : Http.Error -> Html Msg
+viewError error =
+    div []
+        [ h3 [] [ text "There was an error loading the content !" ]
+        , p [] [ text ("Reason : " ++ httpErrorToString error) ]
+        ]
+
+
+httpErrorToString : Http.Error -> String
+httpErrorToString error =
+    case error of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+
+
+-- update and Msg
+
+
+fetchLevels : Cmd Msg
+fetchLevels =
+    Http.get
+        { url = "http://localhost:5019/levels"
+        , expect = Http.expectJson (RemoteData.fromResult >> ReceivedLevels) levelsDecoder
+        }
+
+
+type Msg
+    = ManualLevelLoad
+    | ReceivedLevels (WebData (List Level))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ManualLevelLoad ->
+            ( { model | levels = Loading }, fetchLevels )
+
+        ReceivedLevels levels ->
+            ( { model | levels = levels }, Cmd.none )
