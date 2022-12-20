ff1d8c86b4546dcb4098ca112b9c31eef84c387f
cityhighrider <60808209+city-high-rider@users.noreply.github.com>
2022-12-21 11:13:55 +1300

    add level decoder and rename custom types to avoid confusion

--- src/Level.elm
+++ src/Level.elm
@@ -1,7 +1,10 @@
 -- this is the module for the level type and its associated functions
 
 
-module Level exposing (Level, levelIdToString)
+module Level exposing (Level, levelIdToString, levelsDecoder)
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (optional, required)
 
 
 type alias Level =
@@ -13,8 +16,20 @@ type alias Level =
 
     -- level operators are the things that you can do to your number in the
     -- level, like incrementing or decrementing it.
-    , availableOperators : List (Float -> Float)
+    , availableOperations : List Operation
     }
+
+
+
+-- this custom type is nessecary because our level might have functions that deal with
+-- both ints or floats, like incrementing, or only with ints, like a
+-- factorial function. We also might want to add some function later with
+-- two inputs or something.
+
+
+type Operation
+    = FloatFunction (Float -> Float)
+    | IntFunction (Int -> Int)
 
 
 
@@ -34,3 +49,60 @@ type LevelId
 levelIdToString : LevelId -> String
 levelIdToString (LevelId id) =
     String.fromInt id
+
+
+
+-- json decoder to convert json data to level data type
+
+
+levelsDecoder : Decoder (List Level)
+levelsDecoder =
+    Decode.list levelDecoder
+
+
+levelDecoder : Decoder Level
+levelDecoder =
+    Decode.succeed Level
+        |> required "id" levelIdDecoder
+        |> required "difficulty" Decode.string
+        |> required "startNumber" Decode.float
+        |> required "endNumber" Decode.float
+        |> required "minMovesToPass" Decode.int
+        -- here is the issue : there is no guarantee that the json for the levels will have
+        -- valid functions listed, maybe there is a typo or something.
+        -- so we might have to return an empty list. Later on we can reject the level
+        -- if the list of available operations is empty.
+        |> required "operators" operationsDecoder
+
+
+levelIdDecoder : Decoder LevelId
+levelIdDecoder =
+    Decode.map LevelId Decode.int
+
+
+operationsDecoder : Decoder (List Operation)
+operationsDecoder =
+    -- Our json for the levels will have a list called operators, that 
+    -- contains a bunch of strings which correspond to operations we can
+    -- perform on our number. Unfortunately, we cannot guarantee that 
+    -- the strings in that json list will match to an operation. 
+    -- (see stringToOperation.) Therefore we will have a list of
+    -- "Maybe Operation", and we must filter out the Nothings.
+    -- that is (in theory) what this decoder should do.
+    Decode.map (List.filterMap stringToOperation) (Decode.list Decode.string)
+
+
+stringToOperation : String -> Maybe Operation
+stringToOperation inp =
+    case inp of
+        "increment" ->
+            Just (FloatFunction ((+) 1))
+
+        "decrement" ->
+            Just (FloatFunction ((-) 1))
+
+        "double" ->
+            Just (FloatFunction ((*) 2))
+
+        _ ->
+            Nothing
