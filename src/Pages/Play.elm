a7cc0ae5adb058502e5278680e1e1aa0a42fceaa
cityhighrider <60808209+city-high-rider@users.noreply.github.com>
2022-12-23 09:38:10 +1300

    add move counter and basic win screens

--- src/Pages/Play.elm
+++ src/Pages/Play.elm
@@ -1,7 +1,11 @@
+-- todo : handle rounding explicitly, i.e let the player have a button for
+-- it, add win condition
+
+
 module Pages.Play exposing (..)
 
 import ErrorViewing exposing (viewError)
-import Html exposing (Html, button, div, h2, h3, p, text)
+import Html exposing (Html, button, div, h2, h3, p, strong, text)
 import Html.Events exposing (onClick)
 import Http
 import Inscribed exposing (BindableFunction(..), Inscribed, bind, makeBindable)
@@ -24,6 +28,7 @@ type alias Model =
     -- webdata, since we only initialise it when the level is succesfully
     -- loaded. it is also inscribed so that we can log user action
     , currentNumber : WebData (Inscribed Float)
+    , moves : Int
     }
 
 
@@ -39,6 +44,7 @@ init lvlId =
         initialModel =
             { currentLevel = Loading
             , currentNumber = Loading
+            , moves = 0
             }
     in
     ( initialModel, loadLevel lvlId )
@@ -65,20 +71,64 @@ view model =
             viewError reason
 
         ( Success level, Success number ) ->
-            viewLevel level number
+            viewLevel level number model.moves
 
         ( _, _ ) ->
             h3 [] [ text "Something went wrong..." ]
 
 
-viewLevel : Level -> Inscribed Float -> Html Msg
-viewLevel level number =
+viewLevel : Level -> Inscribed Float -> Int -> Html Msg
+viewLevel level number moves =
     div []
         [ h2 [] [ text ("Level " ++ levelIdToString level.id) ]
         , h3 [] [ text ("Goal number : " ++ String.fromFloat level.goalNumber) ]
         , h3 [] [ text ("Current number : " ++ String.fromFloat (Inscribed.extractValue number)) ]
+        , displayWinCondition level moves number
         , operationButtons level.operations
         , actionHistory number
+        ]
+
+
+displayWinCondition : Level -> Int -> Inscribed Float -> Html Msg
+displayWinCondition level currentMoves (Inscribed.InscribedData currentNumber _) =
+    if currentNumber == level.goalNumber && currentMoves <= level.minMovesToPass then
+        div []
+            [ displayMoves currentMoves
+                level.minMovesToPass
+            , displayWinScreen
+            ]
+
+    else if currentNumber /= level.goalNumber && currentMoves > level.minMovesToPass then
+        div []
+            [ displayMoves currentMoves
+                level.minMovesToPass
+            , displayLoseScreen
+            ]
+
+    else
+        displayMoves currentMoves level.minMovesToPass
+
+
+displayMoves : Int -> Int -> Html Msg
+displayMoves currentMoves allowedMoves =
+    div []
+        [ strong [] [ text ("Remaining moves : " ++ String.fromInt (allowedMoves - currentMoves)) ]
+        ]
+
+
+displayWinScreen : Html Msg
+displayWinScreen =
+    div []
+        [ h3 [] [ text "You won!" ]
+        , button [] [ text "Next ->" ]
+        ]
+
+
+displayLoseScreen : Html Msg
+displayLoseScreen =
+    div []
+        [ h3 [] [ text "You have no more moves left" ]
+        , p [] [ text "Reload the page to retry" ]
         ]
 
 
@@ -161,13 +211,13 @@ update msg model =
                     -- accepts floats or integers
                     case fn of
                         FloatFunction ffn ->
-                            -- if it's a float, we can just chuck our number in. 
+                            -- if it's a float, we can just chuck our number in.
                             -- we do have to wrap it into a remotedata type though
                             ( { model | currentNumber = RemoteData.succeed (bind ffn inscFloat) }, Cmd.none )
 
                         IntFunction ifn ->
                             -- if it's an int, we round our float, put it through,
-                            -- convert the result to float, then remotedata succeed 
+                            -- convert the result to float, then remotedata succeed
                             -- that and save it
                             let
                                 roundedFloat =
@@ -179,7 +229,9 @@ update msg model =
                                 resultToFloat =
                                     Inscribed.map toFloat result
                             in
-                            ({model | currentNumber = RemoteData.succeed resultToFloat}, Cmd.none)
+                            ( { model | currentNumber = RemoteData.succeed resultToFloat }, Cmd.none )
 
                 ( _, _ ) ->
                     ( model, Cmd.none )
+
+
