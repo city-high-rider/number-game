2b731b6730ca3e3458039da6b0fa970e5048aa18
cityhighrider <60808209+city-high-rider@users.noreply.github.com>
2023-01-08 08:59:26 +1300

    modify factorial function to handle negative number case

--- src/MiscMath.elm
+++ src/MiscMath.elm
@@ -1,20 +1,24 @@
 module MiscMath exposing (..)
 
+
 factorial : Int -> Int
 factorial n =
-    if n == 0 then
-        1
+    if n <= 0 then
+        n
 
     else
         n * factorial (n - 1)
+
 
 isRound : Float -> Bool
 isRound x =
     x == (x |> round |> toFloat)
 
+
 square : Float -> Float
 square x =
     x * x
+
 
 half : Float -> Float
 half x =
